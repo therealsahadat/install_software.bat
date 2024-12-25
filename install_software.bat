b50b391241ba9caa6426660e2fcd94357e971657
therealsahadat <123752245+therealsahadat@users.noreply.github.com>
2024-12-25 13:32:37 +0600

    Update install_software.bat

--- install_software.bat
+++ install_software.bat
@@ -1,84 +1,111 @@
 @echo off
 
 echo Installing Mozilla Firefox...
+rem URL: https://www.mozilla.org/en-US/firefox/
 winget install Mozilla.Firefox
 
 echo Installing Microsoft 365 Apps for enterprise...
+rem URL: https://www.microsoft.com/en-us/microsoft-365/get-started-with-office-365
 winget install Microsoft.365
 
 echo Installing PotPlayer...
+rem URL: https://potplayer.daum.net/
 winget install PotPlayer.PotPlayer
 
 echo Installing VLC media player...
+rem URL: https://www.videolan.org/vlc/index.html
 winget install VideoLAN.VLC
 
 echo Installing WinRAR...
+rem URL: https://www.rarlab.com/
 winget install RARLab.WinRAR
 
 echo Installing Zotero...
+rem URL: https://www.zotero.org/
 winget install Zotero.Zotero
 
 echo Installing Zoom...
+rem URL: https://zoom.us/download
 winget install Zoom.Zoom
 
 echo Installing Google Chrome...
+rem URL: https://www.google.com/chrome/
 winget install Google.Chrome
 
 echo Installing NetSpeedMonitor...
+rem URL: https://www.dslreports.com/frontpage
 winget install NetSpeedMonitor.NetSpeedMonitor
 
 echo Installing AnyDesk...
+rem URL: https://anydesk.com/en
 winget install AnyDesk.AnyDesk
 
 echo Installing Avro Keyboard...
+rem URL: https://www.avrokeyboard.com/
 winget install Avro.Keyboard
 
 echo Installing Brave...
+rem URL: https://brave.com/
 winget install Brave.Brave
 
 echo Installing Internet Download Manager...
+rem URL: https://www.internetdownloadmanager.com/
 winget install Internet.Download.Manager
 
 echo Installing Microsoft Edge...
+rem URL: https://www.microsoft.com/en-us/edge
 winget install Microsoft.Edge
 
 echo Installing OBS Studio...
+rem URL: https://obsproject.com/
 winget install OBSProject.OBSStudio
 
 echo Installing qBittorrent...
+rem URL: https://www.qbittorrent.org/
 winget install qBittorrent.qBittorrent
 
 echo Installing Realtek Audio Driver...
+rem URL: https://www.realtek.com/en/
 winget install Realtek.Audio.Driver
 
 echo Installing Canva...
+rem URL: https://www.canva.com/
 winget install Canva.Canva
 
 echo Installing CapCut...
+rem URL: https://www.capcut.com/
 winget install CapCut.CapCut
 
 echo Installing Grammarly for Windows...
+rem URL: https://www.grammarly.com/
 winget install Grammarly.Grammarly
 
 echo Installing Microsoft OneDrive...
+rem URL: https://www.microsoft.com/en-us/microsoft-365/onedrive/download
 winget install Microsoft.OneDrive
 
 echo Installing Spotify...
+rem URL: https://www.spotify.com/
 winget install Spotify.Spotify
 
 echo Installing Telegram Desktop...
+rem URL: https://desktop.telegram.org/
 winget install Telegram.TelegramDesktop
 
 echo Installing Net Monitor...
+rem URL: https://www.netmonitor.com/
 winget install NetMonitor.NetMonitor
 
 echo Installing WhatsApp...
+rem URL: https://www.whatsapp.com/download
 winget install WhatsApp.WhatsApp
 
 echo Installing Microsoft Photos Legacy...
+rem URL: https://www.microsoft.com/en-us/p/microsoft-photos/9wzdncrfjb1q
 winget install Microsoft.PhotosLegacy
 
 echo Installing Snipping Tool...
+rem URL: https://www.microsoft.com/en-us/p/snipping-tool/9msmrpthw9w3
 winget install Microsoft.SnippingTool
 
 echo Installation complete!
